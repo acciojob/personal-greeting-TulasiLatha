b0b867f42e13c42cd458c0aba27444229d68d75c
TulasiLatha <lathagangadhari98@gmail.com>
2023-07-13 00:41:19 +0530

    third

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 const Greet=(()=>{
-const [name,setName]=useState('');
+let [name,setName]=useState('');
 
 const handleChange=(event)=>{
     setName(event.target.value);
